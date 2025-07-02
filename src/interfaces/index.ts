3f997169e01a76f86adcb796e93ebd9765dd46d9
ahmadou5 <ahmadlasauwal@gmail.com>
2025-07-02 03:38:34 +0100

    n

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,19 +1,5 @@
 import { type ApiResponse } from "./api.interface";
-import { type AuthContextProps } from "./context.interface";
-import { type UserInterface } from "./models.interface";
-import { type ReactChildrenProps } from "./react.interface";
-import { type AppContextProps } from "./context.interface";
-import { type MiniContextType } from "./context.interface";
-import { type NetworkContextType } from "./context.interface";
-import { type UserProfile } from "./models.interface";
 
-export {
-  ApiResponse,
-  AppContextProps,
-  NetworkContextType,
-  AuthContextProps,
-  ReactChildrenProps,
-  MiniContextType,
-  UserInterface,
-  UserProfile,
-};
+import { type ReactChildrenProps } from "./react.interface";
+
+export { ApiResponse, ReactChildrenProps };
